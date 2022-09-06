89ddd9336577263846d51215d4719fa62ad46561
Yuset Mercado Vanegas <yusethmercado@gmail.com>
2022-09-06 07:50:30 -0500

    Fin sesion 5

--- src/app/services/infopagina.service.ts
+++ src/app/services/infopagina.service.ts
@@ -10,10 +10,13 @@ import { infoPagina } from '../interfaces/info-pagina.interface';
 export class InfopaginaService {
 
   info: infoPagina= {}
+  equipo: any = [] = [];
   cargada = false;
 
   constructor(private http: HttpClient) { 
-
+    this.CargarInfo();
+    this.CargarEquipo();
+/*
     //console.log('Servicio inicializado y listo para utilizar..!!!!');
     //leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
@@ -35,9 +38,28 @@ export class InfopaginaService {
          .subscribe(resp => { // data is of type HttpResponse<Object>
           console.log(resp.headers.get('X-Custom-Header'));
           console.log(resp.body['someProperty']); //response.body is a JSON         
-         });
+         });*/
+  }
 
-  
+  private CargarInfo(){
+   this.http.get('assets/data/data-pagina.json')
+   .subscribe((resp: infoPagina) => {
+     
+      this.cargada = true;
+      this.info = resp;
+      //console.log(this.info);
+      //console.log(resp['nombre_corto'])
+   });
+  }  
 
+  private CargarEquipo(){
+   this.http.get('https://angular-html-6a672-default-rtdb.firebaseio.com/equipo.json')
+   .subscribe((resp: any[]) => {
+     
+      this.cargada = true;
+      this.equipo = resp;
+      console.log(this.equipo);
+      //console.log(resp['nombre_corto'])
+   });
   }
 }
